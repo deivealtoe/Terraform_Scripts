201c328b2a97b9315dd3272af06140f8140788f1
Deivisson Altoé <deivealtoe@gmail.com>
2022-02-09 21:10:42 -0300

    Adicionado recurso para criar security_group e vincular nas vms

--- getting_started/main.tf
+++ getting_started/main.tf
@@ -7,7 +7,26 @@ resource "aws_instance" "dev" {
     ami = "ami-033b95fb8079dc481"
     instance_type = "t2.micro"
     key_name = "terraform-aws-key"
+
     tags = {
         Name = "dev${count.index}"
     }
+
+    vpc_security_group_ids = ["sg-08148c3ec54bd98cf"]
+}
+
+resource "aws_security_group" "acesso-ssh" {
+  name = "acesso-ssh"
+  description = "acesso-ssh"
+
+  ingress {
+    from_port = 22
+    to_port = 22
+    protocol = "tcp"
+    cidr_blocks = ["168.181.124.128/32"]
+  }
+
+  tags = {
+    Name = "ssh"
+  }
 }
